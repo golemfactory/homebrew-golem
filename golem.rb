6c2f601dae7bc61923b7d96e8a0a452ffa0554e4
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2017-04-01 01:31:33 +0200

    geth temporarily used from the go-ethereum fork

--- golem.rb
+++ golem.rb
@@ -5,16 +5,16 @@ class Golem < Formula
   sha256 'be960690c59e52b6d2494c36d80a50b11efa4f154a30613e401b558689761afd'
   version '0.4.0'
 
-  depends_on 'qt@5.7'
+  depends_on 'qt'
   depends_on 'docker'
   depends_on 'docker-machine'
   depends_on 'xhyve'
   depends_on 'docker-machine-driver-xhyve'
-  depends_on 'ethereum/ethereum/ethereum'
   depends_on 'ipfs'
   depends_on 'openexr'
   depends_on 'freeimage'
   depends_on 'golemfactory/hyperg/hyperg'
+  depends_on 'mfranciszkiewicz/ethereum/ethereum'
 
   def install
 
