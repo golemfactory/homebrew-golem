f877aa045c56d6b115d8c6cb2be33e62b521ccc7
mfranciszkiewicz <mfranciszkiewicz@gmail.com>
2017-08-24 16:18:38 +0200

    Pre-release 0.8.0

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -1,10 +1,10 @@
 cask 'golem' do
-  version '0.1.1'
-  sha256 '876762922b7ba8a9fcdfbeb141abb8e828e311d1ee601e3bf507d3aac5b635e0'
-  url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
+  name 'Golem Worldwide Supercomputer GUI'
+  homepage 'https://github.com/golemfactory/golem-electron/'
 
-  name 'Golem Worldwide Supercomputer'
-  homepage 'https://github.com/golemfactory/golem/'
+  version '0.1.2'
+  sha256 '274cc63dc7d0c5844f93928b3961fa271c2cafc6beff3ef32ecba4afd972abc4'
+  url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
 
   auto_updates true
   depends_on formula: 'golemfactory/golem/golem'
--- golem.rb
+++ golem.rb
@@ -1,34 +1,34 @@
 class Golem < Formula
   desc 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem/'
-  url 'https://github.com/golemfactory/golem/releases/download/0.7.1/golem-macos-0.7.1.tar.gz'
-  sha256 '66b774d7639e59e82d7cc40cc3f8c22f03ffb83042cb777121e54e886ea0f7a2'
-  version '0.7.1'
+
+  version '0.8.0'
+  sha256 'f82642aec0f424057aecabe5ebc43012359390cd31566c8e3fa8ac894ae97ea5'
+  url 'https://github.com/golemfactory/golem/releases/download/0.8.0/golem-macos-0.8.0.tar.gz'
 
   depends_on 'qt'
   depends_on 'docker'
   depends_on 'docker-machine'
   depends_on 'xhyve'
   depends_on 'docker-machine-driver-xhyve'
-  depends_on 'ipfs'
   depends_on 'openexr'
   depends_on 'freeimage'
   depends_on 'golemfactory/hyperg/hyperg'
   depends_on 'openssl'
   depends_on 'ethereum/ethereum/ethereum'
 
-
   def install
+        bin.install 'golemapp'
+        bin.install 'golemcli'
+        bin.install Dir['*']
+  end
 
+  def post_install
     # docker-machine-driver-xhyve need root owner and uid
     # https://github.com/zchee/docker-machine-driver-xhyve
-    system 'echo docker-machine-driver-xhyve need root owner and uid'
-    system "sudo chown root:wheel #{HOMEBREW_PREFIX}/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve"
-    system "sudo chmod u+s #{HOMEBREW_PREFIX}/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve"
 
-    bin.install 'golemapp'
-    bin.install 'golemcli'
-    bin.install Dir['*']
-
+    opoo "\n\n\nPlease execute the following commands manually:
+    sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+    sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve \n\n\n"
   end
 end
