a544292a59cd21f7970047c4c584d201fb6cc2f6
Marek Franciszkiewicz <mfranciszkiewicz@users.noreply.github.com>
2017-10-27 17:45:37 +0200

    Bump core to 0.9.0 and GUI to 0.1.4 (#20)

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -2,8 +2,8 @@ cask 'golem' do
   name 'Golem Worldwide Supercomputer GUI'
   homepage 'https://github.com/golemfactory/golem-electron/'
 
-  version '0.1.3'
-  sha256 '00f4e09d835f0d69648d12eb99062651225f9659a364dd29113053da6e7e0447'
+  version '0.1.4'
+  sha256 'c00749e30da7d0877e8d09246d1e6b4f7c2698a446a11b6bc5475e87d65d35e5'
   url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
 
   auto_updates true
