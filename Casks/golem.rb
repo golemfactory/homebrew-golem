ace1581b5c1f2cdce6b59358f7f07c276f6334ec
Marek Franciszkiewicz <marek@golem.network>
2018-04-09 16:19:05 +0200

    Bump GUI version to 0.1.10

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -2,8 +2,8 @@ cask 'golem' do
   name 'Golem Worldwide Supercomputer GUI'
   homepage 'https://github.com/golemfactory/golem-electron/'
 
-  version '0.1.9'
-  sha256 '30b45a199f813e3b8554a6344e8f112614fe69450957839ab8d8927f5db43a12'
+  version '0.1.10'
+  sha256 '45f7228d162d6656f0e0530040f045d0209f4d166244be0b8fddf387f0cae761'
   url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
 
   auto_updates true
