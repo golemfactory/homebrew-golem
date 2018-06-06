922c179e53c61d92fbe82f780032801df221d25f
Marek Franciszkiewicz <marek@golem.network>
2018-06-06 13:09:39 +0200

    Bump core version to 0.16.0

--- golem.rb
+++ golem.rb
@@ -2,9 +2,9 @@ class Golem < Formula
   desc 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem/'
 
-  version '0.15.1'
-  sha256 '49f087e5ddab8f38121d039c98d7bdc8c2a575ee7078528b7bc4cd27601e6508'
-  url 'https://github.com/golemfactory/golem/releases/download/0.15.1/golem-0.15.1-macOS.tar.gz'
+  version '0.16.0'
+  sha256 'bfbed4aa6311b08b9fa6b3e25f3873b38b67f6da9d1a523741bc12dacf700328'
+  url 'https://github.com/golemfactory/golem/releases/download/0.16.0/golem-0.16.0-macOS.tar.gz'
 
   depends_on 'docker'
   depends_on 'docker-machine'
