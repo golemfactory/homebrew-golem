fc66039bb18867a1f57b70b6f78adc6100786eef
Marek Franciszkiewicz <mfranciszkiewicz@users.noreply.github.com>
2018-06-28 17:31:14 +0200

    Version 0.16.1 (#34)
    * Bump core version to 0.16.1
    
    * Bump GUI version to 0.1.13

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -2,8 +2,8 @@ cask 'golem' do
   name 'Golem Worldwide Supercomputer GUI'
   homepage 'https://github.com/golemfactory/golem-electron/'
 
-  version '0.1.12'
-  sha256 '63200520fa2fcebe94f8fdf7c05b33290b405739d17d9afc417a596a07a6e99f'
+  version '0.1.13'
+  sha256 '9aaff4affb59f9722cac72fc9d3d6a9ac95da041b706ae5a0cf45a6d60dd4f54'
   url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
 
   auto_updates true
--- golem.rb
+++ golem.rb
@@ -2,9 +2,9 @@ class Golem < Formula
   desc 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem/'
 
-  version '0.16.0'
-  sha256 'bfbed4aa6311b08b9fa6b3e25f3873b38b67f6da9d1a523741bc12dacf700328'
-  url 'https://github.com/golemfactory/golem/releases/download/0.16.0/golem-0.16.0-macOS.tar.gz'
+  version '0.16.1'
+  sha256 '89e097e9b3dfd7e04b50158429320997d2a7de9093a2f5bb4699793d6d930719'
+  url 'https://github.com/golemfactory/golem/releases/download/0.16.1/golem-0.16.1-macOS.tar.gz'
 
   depends_on 'docker'
   depends_on 'docker-machine'
