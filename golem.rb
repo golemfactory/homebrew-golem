55b9a0a3836e3afa1303296c9b4e374f63e657e1
Marek Franciszkiewicz <marek@golem.network>
2018-08-21 18:56:57 +0200

    Remove core docker dependency

--- golem.rb
+++ golem.rb
@@ -6,10 +6,6 @@ class Golem < Formula
   sha256 '6d80d6bfc98ccb432b31d82a72a08f2eddaecf398733095e3225ab3accd170e6'
   url 'https://github.com/golemfactory/golem/releases/download/0.17.0/golem-0.17.0-macOS.tar.gz'
 
-  depends_on 'docker'
-  depends_on 'docker-machine'
-  depends_on 'xhyve'
-  depends_on 'docker-machine-driver-xhyve'
   depends_on 'openexr'
   depends_on 'gmp'
   depends_on 'freeimage'
@@ -20,14 +16,5 @@ class Golem < Formula
         bin.install 'golemapp'
         bin.install 'golemcli'
         bin.install Dir['*']
-  end
-
-  def post_install
-    # docker-machine-driver-xhyve need root owner and uid
-    # https://github.com/zchee/docker-machine-driver-xhyve
-
-    opoo "\n\n\nPlease execute the following commands manually:
-    sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-    sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve \n\n\n"
   end
 end
