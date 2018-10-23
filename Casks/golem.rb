7b17fe9f813c388a676aada0d1e736d060bf8e50
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2018-10-23 14:46:50 +0200

    Version 0.18.0 (#43)
    * Cask docker dependency
    
    * Remove core docker dependency
    
    * Create and remove docker binary symlink
    
    * Create docker symlink in 'postflight'
    
    * Version of core & GUI bumped, checksums updated

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -2,13 +2,21 @@ cask 'golem' do
   name 'Golem Worldwide Supercomputer GUI'
   homepage 'https://github.com/golemfactory/golem-electron/'
 
-  version '0.1.15'
-  sha256 'ff2a87c88f4eab35ba86b35230f66123a846ec2cfa4b9a7bcb7fbfa5b13741f2'
+  version '0.1.16'
+  sha256 '1164723e9751b8b6a1ef80e6739a650430ed8462bbda78e446483510f13ab353'
   url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-macos-#{version.major_minor_patch}.dmg"
 
   auto_updates true
   depends_on formula: 'golemfactory/golem/golem'
   depends_on cask: 'golem-mainnet-launcher'
+  depends_on cask: 'docker'
 
   app 'golem.app'
+
+  # Creates a symlink for the newly installed 'docker' binary
+  postflight do
+    FileUtils.ln_sf("/Applications/Docker.app/Contents/Resources/bin/docker", "/usr/local/bin/docker")
+  end
+  # Removes the symlink on deinstallation
+  uninstall delete: "/usr/local/bin/docker"
 end
--- golem.rb
+++ golem.rb
@@ -2,14 +2,10 @@ class Golem < Formula
   desc 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem/'
 
-  version '0.17.1'
-  sha256 '8560b34f16ca64a174eed72ce75a3981ef704d639e853799ed0be952575b7767'
-  url 'https://github.com/golemfactory/golem/releases/download/0.17.1/golem-0.17.1-macOS.tar.gz'
+  version '0.18.0'
+  sha256 '6f353afe5aa1135460db4f2289846e998500c5101663bbdad6d34078d1e2d430'
+  url 'https://github.com/golemfactory/golem/releases/download/0.18.0/golem-0.18.0-macOS.tar.gz'
 
-  depends_on 'docker'
-  depends_on 'docker-machine'
-  depends_on 'xhyve'
-  depends_on 'docker-machine-driver-xhyve'
   depends_on 'openexr'
   depends_on 'gmp'
   depends_on 'freeimage'
@@ -20,14 +16,5 @@ class Golem < Formula
         bin.install 'golemapp'
         bin.install 'golemcli'
         bin.install Dir['*']
-  end
-
-  def post_install
-    # docker-machine-driver-xhyve need root owner and uid
-    # https://github.com/zchee/docker-machine-driver-xhyve
-
-    opoo "\n\n\nPlease execute the following commands manually:
-    sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-    sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve \n\n\n"
   end
 end
