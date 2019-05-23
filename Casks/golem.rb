5c3487a25a2eab94df7c5871bdb4d55f75c27ee8
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2019-05-23 16:56:08 +0200

    Golem & GUI version bump, shasum updated

--- Casks/golem.rb
+++ Casks/golem.rb
@@ -2,8 +2,8 @@ cask 'golem' do
   name 'Golem Worldwide Supercomputer GUI'
   homepage 'https://github.com/golemfactory/golem-electron/'
 
-  version '0.1.19'
-  sha256 '83c10f36ed77c343c5751fc2b4e7dc90bdd0d699400220167dfe779b96bf216f'
+  version '0.1.20'
+  sha256 '2ee853662ddc454a78444ba6022de9bd029ae4bca5a6498bffc3e22747e41eb6'
   url "https://github.com/golemfactory/golem-electron/releases/download/#{version.major_minor_patch}/golem-electron-beta-macos-#{version.major_minor_patch}.dmg"
 
   auto_updates true
