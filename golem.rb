4871c231fbe75581646a2596af582e5955ed30fb
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2020-01-13 12:11:27 +0100

    Version bump, shasum update

--- golem.rb
+++ golem.rb
@@ -2,9 +2,9 @@ class Golem < Formula
   desc 'Golem Worldwide Supercomputer'
   homepage 'https://github.com/golemfactory/golem/'
 
-  version '0.21.0'
-  sha256 'd79efe7a501d7eb43decf10fdd4d0367bd1578dd364d4e79264a51b1e6ff5c3f'
-  url "https://golem-releases.cdn.golem.network/releases/0.21.0/golem-0.21.0-macOS.tar.gz"
+  version '0.22.0'
+  sha256 'a0ca911173d6293bda75cdfa791ca5a1fd311854243010fb638707f282fa5bd0'
+  url "https://golem-releases.cdn.golem.network/releases/0.22.0/golem-0.22.0-macOS.tar.gz"
 
   depends_on 'openexr'
   depends_on 'gmp'
