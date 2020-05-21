077bedfcb569fd9dd2829bfef550389c4d3643f1
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2020-05-21 12:36:15 +0200

    Version bump and shasum update

--- golemcli.rb
+++ golemcli.rb
@@ -3,9 +3,9 @@ class Golemcli < Formula
   homepage 'https://github.com/golemfactory/golem-client'
 
   stable do
-    version '0.3.1'
-    url 'https://golem-releases.cdn.golem.network/releases/0.23.0/components/golemcli-osx-v0.3.1.tar.gz'
-    sha256 'd465243eff42f38bcf758de8467db340793984479a7c57c900025e2491469b61'
+    version '0.3.2'
+    url 'https://golem-releases.cdn.golem.network/releases/0.23.1/components/golemcli-osx-v0.3.2.tar.gz'
+    sha256 'fb2449f1155d34671f7d5885653bbd7b15776903246db9d3e7094bd8a20d5d46'
   end
 
   def install
